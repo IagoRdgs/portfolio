ab6d7c83e27dbf2269da2d2575ab5850ef46c239
HipnosM <iagoandre567@gmail.com>
2024-04-22 22:26:57 -0300

    mobile first: menubar finished

--- src/components/layout/Nav.jsx
+++ src/components/layout/Nav.jsx
@@ -3,7 +3,6 @@ import styles from "./Nav.module.css";
 import { useState } from "react";
 
 import { CiMenuBurger } from "react-icons/ci";
-import { IoClose } from "react-icons/io5";
 
 export default function Nav() {
     const [barActive, setBarActive] = useState();
@@ -11,22 +10,11 @@ export default function Nav() {
     return (
         <div className={styles.header_container}>
             <span className={styles.logo}>Logo</span>
-            {barActive ? (
-                <>
-                    <IoClose
-                        className={`${styles.menu} ${styles.active}`}
-                        onClick={() => {
-                            setBarActive(!barActive);
-                        }}
-                    />
-                    <div className={styles.overlay}></div>
-                </>
-            ) : <CiMenuBurger
-                className={styles.menu}
-                onClick={() => {
-                    setBarActive(!barActive);
-                }}
-            />}
+            <CiMenuBurger
+                className={`${styles.menu} ${barActive ? styles.active : ""}`}
+                onClick={() => { setBarActive(!barActive) }}
+            />
+            {barActive && <div className={styles.overlay}></div>}
             <nav className={barActive ? `${styles.active}` : ""}>
                 <ul>
                     <Navigation
@@ -50,7 +38,9 @@ export default function Nav() {
 const Navigation = ({ href, title }) => {
     return (
         <li>
-            <a href={href} className={styles.Title}>{title}</a>
+            <a href={href}>
+                <span className={styles.title}>{title}</span>
+            </a>
         </li>
     )
 };
