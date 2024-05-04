e2945b88fe049346d2ad395a71eb6f5999135cbd
HipnosM <iagoandre567@gmail.com>
2024-05-04 13:19:20 -0300

    feat: api rest github

--- src/components/pages/Home/sections/projects/Projects.jsx
+++ src/components/pages/Home/sections/projects/Projects.jsx
@@ -1,17 +1,51 @@
 import styles from "./Projects.module.css";
 
-import { Carousel, Container } from "react-bootstrap";
+import { Carousel, CarouselItem, CarouselCaption } from "react-bootstrap";
 import ProjectCard from "./projectCard/ProjectCard";
 
 import projetoimg from "../../../../../assets/hero-programming.svg";
+import { useEffect, useState } from "react";
 
 export default function Projects() {
+
+    const [repoData, setRepoData] = useState([]);
+    const token = "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";
+
+    const apiUrl = "https://api.github.com/users/HipnosM/repos";
+
+    useEffect(() => {
+        fetch(apiUrl, {
+            method: "GET",
+            headers: {
+                Authorization: `token ${token}`,
+                "Content-Type": "application/json",
+            }
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                setRepoData(data);
+                console.log(data);
+            })
+            .catch((err) => console.log(err))
+    }, []);
+
     return (
         <section id="projects" className={styles.project_container}>
             <h2>Projetos</h2>
-            <ProjectCard
-                image={projetoimg}
-            />
+            <Carousel className={styles.carousel} wrap={false} interval={null}>
+
+                {repoData
+                    .map((repo) => (
+                        <CarouselItem>
+                            <ProjectCard
+                                key={repo.id}
+                                projectName={repo.name}
+                                projectImage={projetoimg}
+                            />
+                        </CarouselItem>
+                    ))
+                }
+            </Carousel>
         </section>
     );
 };
--- src/components/pages/Home/sections/projects/projectCard/ProjectCard.jsx
+++ src/components/pages/Home/sections/projects/projectCard/ProjectCard.jsx
@@ -2,17 +2,17 @@ import styles from "./ProjectCard.module.css";
 
 import Button from "../../../../../layout/button/Button";
 
-export default function ProjectCard({ image }) {
+export default function ProjectCard({ projectImage, projectName, projectDesc }) {
     return (
         <section className={styles.card_container}>
             <div className={styles.card}>
                 <div className={styles.card_header}>
-                    <h3>Nome do projeto</h3>
-                    <p>Descrição do projeto Descrição do projetoDescrição do projetoDescrição do projetoDescrição do projetoDescrição do projetoDescrição do projetoDescrição do projetoDescrição do projetoDescrição do projetoDescrição do projetoDescrição do projeto</p>
+                    <h3>{projectName}</h3>
+                    <p>Descrição</p>
                 </div>
 
                 <div className={styles.card_image}>
-                    <img src={image} alt="" />
+                    <img src={projectImage} alt="" />
                 </div>
             </div>
                 <div className={styles.card_buttons}>
