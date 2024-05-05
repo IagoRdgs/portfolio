688f6f4f6c8cf36a13f1e3f6084aaf04bfd31504
HipnosM <iagoandre567@gmail.com>
2024-05-05 19:03:07 -0300

    fix: projects requests from github api

--- src/components/pages/Home/sections/projects/Projects.jsx
+++ src/components/pages/Home/sections/projects/Projects.jsx
@@ -9,40 +9,72 @@ import { useEffect, useState } from "react";
 export default function Projects() {
 
     const [repoData, setRepoData] = useState([]);
-    const token = "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";
+    const [readmeData, setReadmeData] = useState([]);
+
+    const token = import.meta.env.VITE_GITHUB_TOKEN;
 
     const apiUrl = "https://api.github.com/users/HipnosM/repos";
 
-    useEffect(() => {
-        fetch(apiUrl, {
-            method: "GET",
-            headers: {
-                Authorization: `token ${token}`,
-                "Content-Type": "application/json",
+    async function getRepoData() {
+        try {
+            const repoResponse = await fetch(apiUrl, {
+                method: "GET",
+                headers: {
+                    Authorization: `token ${token}`,
+                }
+            });
+
+            if (!repoResponse.ok) {
+                throw new Error('Erro ao buscar os repositórios.');
             }
-        })
-            .then((resp) => resp.json())
-            .then((data) => {
-                setRepoData(data);
-                console.log(data);
-            })
-            .catch((err) => console.log(err))
+
+            const repoData = await repoResponse.json();
+            const repoNames = repoData.map(repo => repo.name);
+
+            const allReadmes = [];
+            for (const repoName of repoNames) {
+                const readmeResponse = await fetch(`https://api.github.com/repos/HipnosM/${repoName}/contents/README.md`, {
+                    method: "GET",
+                    headers: {
+                        Authorization: `token ${token}`,
+                        Accept: "application/vnd.github.raw+json",
+                    }
+                });
+                if (!readmeResponse.ok) {
+                    throw new Error('Erro ao buscar os READMEs.');
+                }
+
+                const readmeData = await readmeResponse.text();
+                allReadmes.push(readmeData);
+                setReadmeData(allReadmes);
+            }
+
+            setRepoData(repoData);
+
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        getRepoData();
     }, []);
 
     return (
         <section id="projects" className={styles.project_container}>
             <h2>Projetos</h2>
             <Carousel className={styles.carousel} wrap={false} interval={null}>
-                {repoData.map((repo) => (
-                        <CarouselItem>
-                            <ProjectCard
-                                key={repo.id}
-                                projectName={repo.name}
-                                projectImage={projetoimg}
-                                codeLink={repo.html_url}
-                            />
-                        </CarouselItem>
-                    ))
+                {repoData.map((repo, id) => (
+                    <CarouselItem key={repo.id}>
+                        <ProjectCard
+                            projectName={repo.name}
+                            projectImage={projetoimg}
+                            codeLink={repo.html_url}
+                            projectLink={repo.homepage}
+                            projectDesc={readmeData[id]}
+                        />
+                    </CarouselItem>
+                ))
                 }
             </Carousel>
         </section>
--- src/components/pages/Home/sections/projects/projectCard/ProjectCard.jsx
+++ src/components/pages/Home/sections/projects/projectCard/ProjectCard.jsx
@@ -2,13 +2,13 @@ import styles from "./ProjectCard.module.css";
 
 import Button from "../../../../../layout/button/Button";
 
-export default function ProjectCard({ projectImage, projectName, codeLink }) {
+export default function ProjectCard({ projectImage, projectName, codeLink, projectLink, projectDesc }) {
     return (
         <section className={styles.card_container}>
             <div className={styles.card}>
                 <div className={styles.card_header}>
                     <h3>{projectName}</h3>
-                    <p>Descrição</p>
+                    <p>{projectDesc}</p>
                 </div>
 
                 <div className={styles.card_image}>
@@ -18,8 +18,8 @@ export default function ProjectCard({ projectImage, projectName, codeLink }) {
                 <div className={styles.card_buttons}>
                     <Button
                         text="Projeto"
-                        href={null}
-                        onClick={() => alert("em desenvolvimento")}
+                        href={projectLink ? projectLink : null}
+                        target="_blank"
                     />
                     <Button
                         text="Código"
