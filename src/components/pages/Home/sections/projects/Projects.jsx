74c4db2d3cf35377e21e001af1f5ef9ae43d0c6c
HipnosM <iagoandre567@gmail.com>
2024-05-27 16:08:25 -0300

    feat: favicon

--- src/components/pages/Home/sections/projects/Projects.jsx
+++ src/components/pages/Home/sections/projects/Projects.jsx
@@ -3,7 +3,6 @@ import styles from "./Projects.module.css";
 import { Carousel, CarouselItem, CarouselCaption } from "react-bootstrap";
 import ProjectCard from "./projectCard/ProjectCard";
 
-import projetoimg from "../../../../../assets/hero-programming.svg";
 import projects from "../../../../../data/projects";
 
 export default function Projects() {
@@ -14,7 +13,7 @@ export default function Projects() {
             <h2>Projetos</h2>
             <div className={styles.carousel_container}>
                 <Carousel className={styles.carousel} wrap={false} interval={null}>
-                    {projects.map((project, id) => (
+                    {projects.map((project) => (
                         <CarouselItem key={project.id}>
                             <ProjectCard
                                 projectName={project.name}
