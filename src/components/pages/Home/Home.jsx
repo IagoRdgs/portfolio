b7686694ca380ad5a740502919753c1ab400a3bf
HipnosM <iagoandre567@gmail.com>
2024-05-31 21:25:18 -0300

    fix: scroll no mobile

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -8,16 +8,22 @@ import About from "./sections/About/About";
 import Skills from "./sections/Skills/Skills";
 import Projects from "./sections/projects/Projects";
 import { useEffect } from "react";
+import Contact from "./sections/Contact/Contact";
 
 export default function Home() {
     useEffect(() => {
+        let lastTouchY = 0;
         let lastScrollTop = 0;
         const navbar = document.querySelector('header');
 
-        const handleScroll = () => {
-            let scrollTop = window.pageYOffset || window.scrollY || document.documentElement.scrollTop;
+        const handleTouchMove = (event) => {
+            lastTouchY = event.touches[0].clientY;
+        }
 
-            if (scrollTop > lastScrollTop) {
+        const handleScroll = () => {
+            let scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+            if (scrollTop > lastScrollTop || lastTouchY > 0) {
                 // Scrolling down
                 navbar.style.top = '-70px'; // Ajuste a altura da navbar
             } else {
@@ -25,11 +31,14 @@ export default function Home() {
                 navbar.style.top = '0';
             }
             lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // Para o caso de scroll negativo no mobile
+            lastTouchY = 0;
         };
 
+        window.addEventListener('touchmove', handleTouchMove, { passive: true });
         window.addEventListener('scroll', handleScroll);
 
         return () => {
+            window.removeEventListener('touchmove', handleTouchMove);
             window.removeEventListener('scroll', handleScroll);
         };
     }, []);
@@ -46,6 +55,9 @@ export default function Home() {
                     <Skills />
                     <Projects />
                 </main>
+                <footer>
+                    <Contact />
+                </footer>
 
             </Container>
 
