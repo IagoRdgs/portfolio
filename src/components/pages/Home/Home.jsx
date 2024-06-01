0883b43afde8f5c8e4c16d347f7e714757349ced
HipnosM <iagoandre567@gmail.com>
2024-05-31 21:25:18 -0300

    fix: scroll no mobile

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -11,13 +11,18 @@ import { useEffect } from "react";
 
 export default function Home() {
     useEffect(() => {
+        let lastTouchY = 0;
         let lastScrollTop = 0;
         const navbar = document.querySelector('header');
+
+        const handleTouchMove = (event) => {
+            lastTouchY = event.touches[0].clientY;
+        }
 
         const handleScroll = () => {
             let scrollTop = window.pageYOffset || window.scrollY || document.documentElement.scrollTop;
 
-            if (scrollTop > lastScrollTop) {
+            if (scrollTop > lastScrollTop && lastTouchY > 0) {
                 // Scrolling down
                 navbar.style.top = '-70px'; // Ajuste a altura da navbar
             } else {
@@ -25,11 +30,14 @@ export default function Home() {
                 navbar.style.top = '0';
             }
             lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // Para o caso de scroll negativo no mobile
+            lastTouchY = 0;
         };
 
+        window.addEventListener('touchmove', handleTouchMove, { passive: true });
         window.addEventListener('scroll', handleScroll);
 
         return () => {
+            window.removeEventListener('touchmove', handleTouchMove);
             window.removeEventListener('scroll', handleScroll);
         };
     }, []);
