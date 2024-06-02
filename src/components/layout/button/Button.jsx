aad1bbf113a550d743d579f94b1f7de75511c95c
HipnosM <iagoandre567@gmail.com>
2024-06-01 22:43:17 -0300

    feat: página de contato + fix: bug hover do button

--- src/components/layout/button/Button.jsx
+++ src/components/layout/button/Button.jsx
@@ -2,6 +2,8 @@ import styles from "./Button.module.css";
 
 export default function Button({text, href, download, target, onClick}) {
     return (
-        <a className={styles.button} href={href} target={target} download={download} onClick={onClick}>{text}</a>
+        <div className={styles.button}>
+            <a href={href} target={target} download={download} onClick={onClick}>{text}</a>
+        </div>
     );
 };
--- src/components/pages/Home/sections/Contact/Contact.jsx
+++ src/components/pages/Home/sections/Contact/Contact.jsx
@@ -1,7 +1,32 @@
+import styles from "../Contact/Contact.module.css";
+
+import { MdEmail, MdWhatsapp } from "react-icons/md";
+
+import { Container } from "react-bootstrap";
+import { FaInstagram, FaLinkedin } from "react-icons/fa";
+
 export default function Contact() {
     return (
-        <section>
-            
+        <section id="contact" className={styles.contact_container}>
+            <Container>
+                <h2>Contato</h2>
+                <div className={styles.contact_content}>
+                    <p>Envie-me um E-mail</p>
+                    <form action="https://formspree.io/f/mgegjaqw" method="POST" className={styles.form_contact}>
+                        <div className={styles.input_group}>
+                            <input type="email" id="email" name="email" placeholder="exemplo@email.com" required />
+                            <label htmlFor="email">E-mail</label>
+                        </div>
+
+                        <div className={styles.input_group}>
+                            <input type="text" name="message" id="message" placeholder="Sua mensagem..." required></input>
+                            <label htmlFor="message">Mensagem</label>
+                        </div>
+
+                        <button type="submit">Enviar</button>
+                    </form>
+                </div>
+            </Container>
         </section>
     );
 };
