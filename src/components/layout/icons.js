f782703b53a5ed26cc5c6ec6e93d760e55c94420
IagoRdgs <iagoandre567@gmail.com>
2025-09-24 23:08:33 -0300

    :sparkles: feat: mapeamento dos ícones das skills

---
+++ src/components/layout/icons.js
@@ -0,0 +1,21 @@
+import { FaCss3Alt, FaDatabase, FaHtml5, FaJs, FaReact } from "react-icons/fa";
+import { TbBrandReactNative } from "react-icons/tb";
+import { SiAxios, SiExpress, SiGit, SiNodedotjs, SiPrisma, SiTailwindcss, SiTypescript } from "react-icons/si";
+
+const iconMap = {
+    FaHtml5,
+    FaCss3Alt,
+    FaJs,
+    SiNodedotjs,
+    FaReact,
+    TbBrandReactNative,
+    SiAxios,
+    SiExpress,
+    SiGit,
+    SiPrisma,
+    SiTailwindcss,
+    SiTypescript,
+    FaDatabase
+};
+
+export default iconMap;
