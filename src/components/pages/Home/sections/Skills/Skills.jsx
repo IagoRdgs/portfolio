3d3e7b39991e04497cea481344cef7400b154c9b
IagoRdgs <iagoandre567@gmail.com>
2025-10-17 23:23:03 -0300

    :recycle: style: removidas as barras das skills + ajustes na animação no mobile

--- src/components/pages/Home/sections/Skills/Skills.jsx
+++ src/components/pages/Home/sections/Skills/Skills.jsx
@@ -23,7 +23,7 @@ export default function Skills() {
     }
 
     return (
-        <motion.section initial={{ x: -400, opacity: 0 }} whileInView={{ x: 0, opacity: 1, }} transition={{ duration: 1 }} viewport={{ once: true }} id="skills" className={styles.skills_container}>
+        <motion.section initial={{ x: -200, opacity: 0 }} whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 1 }} viewport={{ once: true }} id="skills" className={styles.skills_container}>
             <Container>
                 <h2>Skills</h2>
                 <div className={styles.skills} >
@@ -35,7 +35,6 @@ export default function Skills() {
                                     key={index}
                                     icon={IconComponent ? <IconComponent /> : null}
                                     skill={item.title}
-                                    value={item.value}
                                     customClass={`color_${item.customClass}`}
                                     index={index}
                                 />
@@ -50,16 +49,12 @@ export default function Skills() {
     );
 };
 
-const SkillItem = ({ icon, skill, customClass, value, index }) => {
+const SkillItem = ({ icon, skill, customClass, index }) => {
     return (
         <motion.div initial={{ opacity: 0, y: 100, rotateZ: 15 }} whileInView={{ opacity: 1, y: 0, rotateZ: 0 }} transition={{ duration: .5, delay: index * .15 }} viewport={{ once: true }} className={`${styles.skill_card} ${styles[customClass]}`}>
             <div className={styles.card_content}>
                 {icon}
                 <p className={styles.skill_skill}>{skill}</p>
-            </div>
-
-            <div className={styles.bar_box}>
-                <div className={styles.bar} style={{ width: `${value}%` }}></div>
             </div>
         </motion.div>
     );
--- src/components/pages/Home/sections/projects/projectCard/ProjectCard.jsx
+++ src/components/pages/Home/sections/projects/projectCard/ProjectCard.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 
 export default function ProjectCard({ project, onClick, index }) {
     return (
-        <motion.div initial={{ opacity: 0, scale: 0.1, x: -200 }} whileInView={{ opacity: 1, scale: 1, x: 0 }} transition={{ duration: .5, delay: .2 * index }} viewport={{ once: true }} className={styles.card_container} onClick={onClick}>
+        <motion.div initial={{ opacity: 0, scale: 0.1, x: -150 }} whileInView={{ opacity: 1, scale: 1, x: 0 }} transition={{ duration: .5, delay: .2 * index }} viewport={{ once: true }} className={styles.card_container} onClick={onClick}>
             <img
                 src={project.imageUrl}
                 alt={project.name}
